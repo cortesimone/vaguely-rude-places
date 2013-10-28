71d46f5ce79f1404b7672071503b3b84472daa9f
sabas <sabas88@gmail.com>
2013-10-28 16:21:13 +0100

    Update rude-geojson.js

--- js/rude-geojson.js
+++ js/rude-geojson.js
@@ -4,7 +4,7 @@
 // When adding new places, don't create an "id" property ... this is done by ../tools/assign-ids.php
 
 var places = {
-    "last_id": "34",
+    "last_id": "38",
     "type": "FeatureCollection",
     "features": [
         {
